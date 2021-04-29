3d6bf8671f7cd7b07068b810070fcd042afdf2d0
Axosolaman <70807261+axoguys@users.noreply.github.com>
2021-04-29 20:27:57 +0600

    its  that script
    it  will install all tool that

---
+++ basic.sh
@@ -0,0 +1,15 @@
+
+apt update -y
+apt ugrade -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+git clone https://github.com/rajkumardusad/Tool-X.git
+git clone https://github.com/htr-tech/nexphisher.git
+pip install lolcat
+git clone https://github.com/noob-hackers/tunnel.git
+apt install nano -y
+apt install ruby -y
+apt install php -y
+pip install perl 
+git clone https://github.com/noob-hackers/m-wiz.git
